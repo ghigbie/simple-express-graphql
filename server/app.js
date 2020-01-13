dea505553e4601b9813d4db4aa07c027e3e98dbd
ghigbie <gehigbie@gmail.com>
2020-01-12 17:31:04 -0700

    Edit tasks

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+}))
+
 
 const PORT = 4000;
 app.listen(PORT, () => {
